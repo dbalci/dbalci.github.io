a1cb04acd128288daf3105341470ad8356e04b5b
Dilan Balci <dlanbalci@gmail.com>
2020-05-22 23:41:01 -0700

    Added icons and links.

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 // alert("Way anasini")
+
+
+function CopyEamil() {
+    let arr = ['m','o','c','.','l','i','a','m','g','@','i','c','l','a','b','n','a','l','d']
+    navigator.clipboard.writeText(arr.reverse().join())
+}
